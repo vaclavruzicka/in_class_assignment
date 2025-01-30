733bab99f206e548645eae441f03f3d1e3615e8b
Vaclav Ruzicka <vaclavruzicka@usf.edu>
2025-01-30 16:16:29 -0500

    Practice Problem 1 & 2

--- in_class_assignment_1.js
+++ in_class_assignment_1.js
@@ -1,2 +1,20 @@
 // Task 1
+let x = 10;
+if (x > 5) {
+    console.log("Help");
+}
+//Practice Problem # 1
+let expenses = 10000;
+if (expenses > 7000) {
+    console.log("High Expenses");
+} else {
+    console.log("Manageable Expenses");
+};
 
+//Practice Problem #2
+let student_score = 85;
+if (student_score > 60) {
+    console.log("Passed");
+} else {
+    console.log("Failed");
+}
